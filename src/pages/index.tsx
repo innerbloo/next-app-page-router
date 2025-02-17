b43d9f58cf8701cfaec8a63c0cf92a20d5c61df2
Hosung <nsa19020@naver.com>
2025-02-17 21:22:06 +0900

    SSR 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,44 @@
+import { InferGetServerSidePropsType } from 'next';
 import { ReactNode } from 'react';
 
 import style from './index.module.css';
 
 import BookItem from '@/components/book-item';
 import SearchableLayout from '@/components/searchable-layout';
-import books from '@/mock/books.json';
+import fetchBooks from '@/lib/fetch-books';
+import fetchRandomBooks from '@/lib/fetch-random-books';
 
-export default function Home() {
+export const getServerSideProps = async () => {
+    // const allBooks = await fetchBooks();
+    // const recoBooks = await fetchRandomBooks();
+    const [allBooks, recoBooks] = await Promise.all([
+        fetchBooks(),
+        fetchRandomBooks(),
+    ]);
+
+    return {
+        props: {
+            allBooks,
+            recoBooks,
+        },
+    };
+};
+
+export default function Home({
+    allBooks,
+    recoBooks,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
     return (
         <div className={style.container}>
             <section>
                 <h3>지금 추천하는 도서</h3>
-                {books.map((book) => (
+                {recoBooks.map((book) => (
                     <BookItem key={book.id} {...book} />
                 ))}
             </section>
             <section>
                 <h3>등록된 모든 도서</h3>
-                {books.map((book) => (
+                {allBooks.map((book) => (
                     <BookItem key={book.id} {...book} />
                 ))}
             </section>
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,20 +1,32 @@
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 import { useRouter } from 'next/router';
 import { ReactNode } from 'react';
 
 import BookItem from '@/components/book-item';
 import SearchableLayout from '@/components/searchable-layout';
-import books from '@/mock/books.json';
+import fetchBooks from '@/lib/fetch-books';
 
-export default function Search() {
+export const getServerSideProps = async (
+    context: GetServerSidePropsContext,
+) => {
+    const q = context.query.q;
+    const books = await fetchBooks(q as string);
+
+    return {
+        props: {
+            books,
+        },
+    };
+};
+
+export default function Search({
+    books,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
     const router = useRouter();
     const { q } = router.query;
 
     return (
-        <div>
-            {books.map((book) => (
-                <BookItem key={book.id} {...book} />
-            ))}
-        </div>
+        <div>{books?.map((book) => <BookItem key={book.id} {...book} />)}</div>
     );
 }
 
